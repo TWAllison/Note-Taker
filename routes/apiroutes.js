1779f63466caf1595d0855d6c1e8e0c764cca058
TWAllison <tylerallison55@gmail.com>
2021-12-20 04:58:08 -0700

    set up api router with GET and POST use fs to write new file to db.json module.exports to export

---
+++ routes/apiroutes.js
@@ -0,0 +1,28 @@
+const router = require("express").router();
+const fs = require("fs");
+const { json } = require("express");
+let database = require("../db/db.json");
+
+// GET route
+router.get("/notes", function (req, res) {
+  database = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  res.json(database);
+});
+
+// POST route
+router.post("/notes", function (req, res) {
+  let newNote = {
+    title: req.body.title,
+    text: req.body.text,
+    id: Math.floor(Math.random() * 1000), // store note with randomly generated id
+  };
+
+  // push a new note to db.json with fs
+  database.push(newNote);
+  fs.writeFileSync("./db/db.json", JSON.stringify(database));
+  res.json(database);
+});
+
+// export module
+
+module.exports = router;
