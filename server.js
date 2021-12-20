e6494cceb5580e35e38f18958e8f576206b128ac
TWAllison <tylerallison55@gmail.com>
2021-12-20 04:27:19 -0700

    set up server.js with dependencies and create port on 3001

---
+++ server.js
@@ -0,0 +1,18 @@
+// add all required routes and packages 
+const express = require('express');
+const app = express();
+const apiroutes = require('./routes/apiroutes');
+const htmlroutes = require('./routes/htmlroutes');
+const path = require('path');
+
+// port creation 
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({extend: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api', apiroutes);
+app.use('/', htmlroutes);
+
+// start server on port 
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
